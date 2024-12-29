d590f45be15792d05f60066912c205704cfb25f7
Kim Junsik <121302951+MatchaKim@users.noreply.github.com>
2024-12-29 16:47:07 +0900

    Update template.sh

--- template.sh
+++ template.sh
@@ -43,7 +43,7 @@ ENTITY_PACKAGE="$PACKAGE.$ENTITY_LOWER"
 # 파일 템플릿 정의
 ENTITY_TEMPLATE="package $ENTITY_PACKAGE.entity;
 
-import javax.persistence.*;
+import jakarta.persistence.*;
 
 @Entity
 public class $ENTITY_PASCAL {
@@ -75,26 +75,7 @@ public interface ${ENTITY_PASCAL}Repository extends JpaRepository<$ENTITY_PASCAL
 
 SERVICE_TEMPLATE="package $ENTITY_PACKAGE.service;
 
-import $ENTITY_PACKAGE.dto.${ENTITY_PASCAL}Dto;
-import java.util.List;
-
-public interface ${ENTITY_PASCAL}Service {
-
-    List<${ENTITY_PASCAL}Dto> getAll${ENTITY_PASCAL}s();
-
-    ${ENTITY_PASCAL}Dto get${ENTITY_PASCAL}ById(Long id);
-
-    ${ENTITY_PASCAL}Dto create${ENTITY_PASCAL}(${ENTITY_PASCAL}Dto ${ENTITY_LOWER}Dto);
-
-    ${ENTITY_PASCAL}Dto update${ENTITY_PASCAL}(Long id, ${ENTITY_PASCAL}Dto ${ENTITY_LOWER}Dto);
-
-    void delete${ENTITY_PASCAL}(Long id);
-}"
-
-SERVICE_IMPL_TEMPLATE="package $ENTITY_PACKAGE.service;
-
-import $ENTITY_PACKAGE.dto.${ENTITY_PASCAL}Dto;
-import $ENTITY_PACKAGE.entity.$ENTITY_PASCAL;
+import $ENTITY_PACKAGE.entity.${ENTITY_PASCAL};
 import $ENTITY_PACKAGE.repository.${ENTITY_PASCAL}Repository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -102,78 +83,74 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class ${ENTITY_PASCAL}ServiceImpl implements ${ENTITY_PASCAL}Service {
+public class ${ENTITY_PASCAL}Service {
 
     @Autowired
     private ${ENTITY_PASCAL}Repository repository;
 
-    @Override
-    public List<${ENTITY_PASCAL}Dto> getAll${ENTITY_PASCAL}s() {
-        return repository.findAll();
-    }
+    // public List<${ENTITY_PASCAL}> getAll${ENTITY_PASCAL}s() {
+    //     return CommonResponse.success(repository.findAll());
+    // }
 
-    @Override
-    public ${ENTITY_PASCAL}Dto get${ENTITY_PASCAL}ById(Long id) {
-        return repository.findById(id).orElse(null);
-    }
+    // public ${ENTITY_PASCAL} get${ENTITY_PASCAL}ById(Long id) {
+    //     return CommonResponse.success(repository.findById(id).orElse(null));
+    // }
 
-    @Override
-    public ${ENTITY_PASCAL}Dto create${ENTITY_PASCAL}(${ENTITY_PASCAL}Dto ${ENTITY_LOWER}Dto) {
-        return repository.save(${ENTITY_LOWER}Dto);
-    }
+    // public ${ENTITY_PASCAL} create${ENTITY_PASCAL}(${ENTITY_PASCAL} ${ENTITY_LOWER}) {
+    //     return CommonResponse.success(repository.save(${ENTITY_LOWER}));
+    // }
 
-    @Override
-    public ${ENTITY_PASCAL}Dto update${ENTITY_PASCAL}(Long id, ${ENTITY_PASCAL}Dto ${ENTITY_LOWER}Dto) {
-        // 로직 구현
-        return repository.save(${ENTITY_LOWER}Dto);
-    }
+    // public ${ENTITY_PASCAL} update${ENTITY_PASCAL}(Long id, ${ENTITY_PASCAL} ${ENTITY_LOWER}) {
+    //     return CommonResponse.success(repository.save(${ENTITY_LOWER}));
+    // }
 
-    @Override
-    public void delete${ENTITY_PASCAL}(Long id) {
-        repository.deleteById(id);
-    }
+    // public void delete${ENTITY_PASCAL}(Long id) {
+    //     repository.deleteById(id);
+    // }
 }"
 
 CONTROLLER_TEMPLATE="package $ENTITY_PACKAGE.controller;
 
+import $ENTITY_PACKAGE.entity.${ENTITY_PASCAL};
 import $ENTITY_PACKAGE.dto.${ENTITY_PASCAL}Dto;
 import $ENTITY_PACKAGE.service.${ENTITY_PASCAL}Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import kr.goraes.modulecommon.commonResponse.CommonResponse;
 
 import java.util.List;
 
 @RestController
-@RequestMapping(\"/${ENTITY_LOWER}s\")
+@RequestMapping(\"/${ENTITY_LOWER}\")
 public class ${ENTITY_PASCAL}Controller {
 
     @Autowired
     private ${ENTITY_PASCAL}Service service;
 
-    @GetMapping
-    public List<${ENTITY_PASCAL}Dto> getAll${ENTITY_PASCAL}s() {
-        return service.getAll${ENTITY_PASCAL}s();
-    }
+    // @GetMapping
+    // public ResponseEntity<CommonResponse> getAll${ENTITY_PASCAL}s() {
+    //     return service.getAll${ENTITY_PASCAL}s();
+    // }
 
-    @GetMapping(\"/{id}\")
-    public ${ENTITY_PASCAL}Dto get${ENTITY_PASCAL}ById(@PathVariable Long id) {
-        return service.get${ENTITY_PASCAL}ById(id);
-    }
+    // @GetMapping(\"/{id}\")
+    // public ResponseEntity<CommonResponse> get${ENTITY_PASCAL}ById(@PathVariable Long id) {
+    //     return service.get${ENTITY_PASCAL}ById(id);
+    // }
 
-    @PostMapping
-    public ${ENTITY_PASCAL}Dto create${ENTITY_PASCAL}(@RequestBody ${ENTITY_PASCAL}Dto ${ENTITY_LOWER}Dto) {
-        return service.create${ENTITY_PASCAL}(${ENTITY_LOWER}Dto);
-    }
+    // @PostMapping
+    // public ResponseEntity<CommonResponse> create${ENTITY_PASCAL}(@RequestBody ${ENTITY_PASCAL} ${ENTITY_LOWER}) {
+    //     return service.create${ENTITY_PASCAL}(${ENTITY_LOWER});
+    // }
 
-    @PutMapping(\"/{id}\")
-    public ${ENTITY_PASCAL}Dto update${ENTITY_PASCAL}(@PathVariable Long id, @RequestBody ${ENTITY_PASCAL}Dto ${ENTITY_LOWER}Dto) {
-        return service.update${ENTITY_PASCAL}(id, ${ENTITY_LOWER}Dto);
-    }
+    // @PutMapping(\"/{id}\")
+    // public ResponseEntity<CommonResponse> update${ENTITY_PASCAL}(@PathVariable Long id, @RequestBody ${ENTITY_PASCAL} ${ENTITY_LOWER}) {
+    //     return service.update${ENTITY_PASCAL}(id, ${ENTITY_LOWER});
+    // }
 
-    @DeleteMapping(\"/{id}\")
-    public void delete${ENTITY_PASCAL}(@PathVariable Long id) {
-        service.delete${ENTITY_PASCAL}(id);
-    }
+    // @DeleteMapping(\"/{id}\")
+    // public void delete${ENTITY_PASCAL}(@PathVariable Long id) {
+    //     service.delete${ENTITY_PASCAL}(id);
+    // }
 }"
 
 # 경로 입력 없이 필요한 경로와 클래스 파일을 자동으로 생성
@@ -181,7 +158,6 @@ create_file "./$ENTITY_LOWER/entity" "${ENTITY_PASCAL}.java" "$ENTITY_TEMPLATE"
 create_file "./$ENTITY_LOWER/dto" "${ENTITY_PASCAL}Dto.java" "$DTO_TEMPLATE"
 create_file "./$ENTITY_LOWER/repository" "${ENTITY_PASCAL}Repository.java" "$REPOSITORY_TEMPLATE"
 create_file "./$ENTITY_LOWER/service" "${ENTITY_PASCAL}Service.java" "$SERVICE_TEMPLATE"
-create_file "./$ENTITY_LOWER/service" "${ENTITY_PASCAL}ServiceImpl.java" "$SERVICE_IMPL_TEMPLATE"
 create_file "./$ENTITY_LOWER/controller" "${ENTITY_PASCAL}Controller.java" "$CONTROLLER_TEMPLATE"
 
 echo "Spring 보일러플레이트 코드 생성 완료!"
